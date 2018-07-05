f052c74a150753f007e7374da95fea0081828379
rtopz <rtopz@users.noreply.github.com>
2018-07-04 20:20:48 -0400

    Add sensor1 object

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include "stepper.h"
+#include "sensor.h"
 
 // Define details about motor #1
 #define		m1_stepsPerRev			200		// Sets the motor's steps per 1 revolution (360deg)
@@ -10,6 +11,7 @@
 #define		s1_sigPin		1
 
 stepper * motor1;
+sensor * sensor1;
 int count;
 
 void setup() {
@@ -17,6 +19,10 @@ void setup() {
 
 	Serial.println("Initialize stepper motor 1");
 	motor1 = new stepper(m1_dirPin, m1_stepPin, s1_sigPin, m1_stepsPerRev);
+
+	Serial.println("Initialize sensor 1");
+	sensor1 = new sensor(s1_sigPin);
+
 	Serial.println("Homing motor 1");
 	motor1->runSensorHoming();
 }
